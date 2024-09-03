654a009f8fa3636ec63bbe4fa46d630483d82532
Tanvir Ahamed Ome <17-34279-1@student.aiub.edu>
2024-09-04 03:26:08 +0600

    update images path

--- typescript/core/GenerateGitHubCardsService.ts
+++ typescript/core/GenerateGitHubCardsService.ts
@@ -32,10 +32,8 @@ export class GenerateGitHubCardsService {
     };
 
     // Paths to the input SVG file and the output file
-    // const inputSVGPath = path.resolve('images/github_stats_card_template.svg');
-    // const outputSVGPath = path.resolve('images/github_stats_card.svg');
-    const inputSVGPath = '../../images/github_stats_card_template.svg';
-    const outputSVGPath = '../../images/github_stats_card.svg';
+    const inputSVGPath = path.resolve(__dirname, '../../images/github_stats_card_template.svg');
+    const outputSVGPath = path.resolve(__dirname, '../../images/github_stats_card.svg');
 
     const svgProcessor = new SVGProcessor();
     await svgProcessor.processSVG(inputSVGPath, outputSVGPath, templateValues);
@@ -43,10 +41,8 @@ export class GenerateGitHubCardsService {
 
   private async generateGitHubLanguagesCard(): Promise<void> {
     // Paths to the input SVG file and the output file
-    // const inputSVGPath = path.resolve('images/github_languages_card_template.svg');
-    // const outputSVGPath = path.resolve('images/github_languages_card.svg');
-    const inputSVGPath = '../../images/github_languages_card_template.svg';
-    const outputSVGPath = '../../images/github_languages_card.svg';
+    const inputSVGPath = path.resolve(__dirname, '../../images/github_languages_card_template.svg');
+    const outputSVGPath = path.resolve(__dirname, '../../images/github_languages_card.svg');
     const languages = this.githubService.getLanguages();
     const animationDelay = 100;
     const svgProcessor = new SVGProcessor();
