d30eb6349ebb8ac9f3022643868095c0f2211c16
Tanvir Ahamed Ome <17-34279-1@student.aiub.edu>
2024-09-04 02:58:15 +0600

    update index.ts file path

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,6 +1,6 @@
-import { GenerateGitHubCardsService } from './typescript/core/GenerateGitHubCardsService';
-import { GitHubService } from './typescript/core/GitHubService';
-import { GitHubClient } from './typescript/core/GitHubClient';
+import { GenerateGitHubCardsService } from './core/GenerateGitHubCardsService';
+import { GitHubService } from './core/GitHubService';
+import { GitHubClient } from './core/GitHubClient';
 
 (async () => {
   const token = process.env.ACCESS_TOKEN; // Set your token in environment variable
@@ -15,7 +15,6 @@ import { GitHubClient } from './typescript/core/GitHubClient';
 
   try {
     await gitHubService.fetchAndSetUserDetails();
-
     await generateGitHubCardService.generateCards();
   } catch (error) {
     console.error('Error fetching user details:', error);
